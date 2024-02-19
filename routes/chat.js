375a6657f81a57aeff3b9638c1f84e0bd109434f
Durgesh Thakur <durgesh.bisen10102@gmail.com>
2024-02-19 16:45:50 +0530

    add route for save chat

--- routes/chat.js
+++ routes/chat.js
@@ -2,8 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const Router = express.Router();
 
+const Chat = require('../db/scheema/Chat')
+
 const auth = require("../middleware/auth");
- 
+
 Router.get("/chat", auth.isLogin, (req, res) => {
     const user = req.session.user;
     const userData = {
@@ -11,13 +13,28 @@ Router.get("/chat", auth.isLogin, (req, res) => {
         phone: user.phone,
         profileImage: user.profileImage
     };
-    if(!userData){
+    if (!userData) {
         res.redirect("/")
     }
     if (!user) {
         res.redirect("/home");
     } else {
         res.render("./chat/chat", { userData });
+    }
+});
+
+Router.post("/save_chat", auth.isLogin, async (req, res) => {
+    try {
+        let chat = new Chat({
+            sender: req.body.userId,
+            receiver: req.body.receiver_id,
+            message: req.body.message
+        });
+        const newChat = await chat.save();
+        res.status(200).send({ success: true, msg: 'Chat saved successfully!', data: newChat });
+    } catch (error) {
+        console.error("Error saving chat:", error);
+        res.status(500).send({ success: false, msg: 'An error occurred while saving the chat.' });
     }
 });
 
